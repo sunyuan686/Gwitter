2dd9d4fb90a2f6e480790de6240c534878628b04
SimonAKing <hi@simonaking.com>
2025-06-03 00:14:28 +0800

    fix: 修复彩蛋文本问题

--- src/i18n/locales/en.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  egg: {
-    message: 'Thanks for visiting!',
-    hope: 'Hope to see you again',
-    comment: '// TODO: Add more interesting content',
-    runCode: 'Run Code'
-  }
-};
