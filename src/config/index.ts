7de40b8592cd928c604bcf8f51abd69d6264ad72
SimonAKing <hi@simonaking.com>
2025-06-29 17:52:28 +0800

    fix: 线上 oauth 认证失败问题

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 const config = {
   token: ['9c48ed2297d7d9bf9447', '6de723dbf1a6e4adeacd'],
   owner: 'SimonAKing',
@@ -5,8 +7,10 @@ const config = {
   pageSize: 6,
   offsetTop: 1,
   avatar: 'https://cdn.jsdelivr.net/gh/SimonAKing/images/blog/avatar.jpg',
-  clientID: '56af6ab05592f0a2d399',
-  clientSecret: '5d7e71a1b6130001e84956420ca5b88bc45b7d3c',
+  clientID: isDev ? '56af6ab05592f0a2d399' : '694df1779e48d5a450d3',
+  clientSecret: isDev
+    ? '5d7e71a1b6130001e84956420ca5b88bc45b7d3c'
+    : '23420dd29f671adc5107a5565ed47f655f8e1260',
   autoProxy:
     'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
 };
